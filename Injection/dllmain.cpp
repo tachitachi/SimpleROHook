0eafa586aa284f3cb9e1f834b52e16b687b30050
Niklas K <drdaxxy@googlemail.com>
2018-01-25 11:31:04 +0100

    use minhook for hooking functions without hotpatch structure

--- Injection/dllmain.cpp
+++ Injection/dllmain.cpp
@@ -8,10 +8,63 @@
 
 #include "Core/RoCodeBind.h"
 
+static BOOL g_useMinHook = TRUE;
 
 BOOL InstallProxyFunction(LPCTSTR dllname,LPCSTR exportname,VOID *ProxyFunction,LPVOID *pOriginalFunction)
 {
 	BOOL result = FALSE;
+
+	DEBUG_LOGGING_NORMAL(("InstallProxyFunction(%s:%s)", dllname, exportname));
+
+	if (g_useMinHook)
+	{
+		LPVOID ppTarget;
+		LPWSTR dllnameW;
+#ifdef UNICODE
+		// this is kinda silly since none of the other code cares about supporting DUNICODE but hey
+		dllnameW = dllname;
+#else
+		int len = MultiByteToWideChar(CP_ACP, 0, dllname, -1, NULL, 0);
+		if (len == 0)
+		{
+			DEBUG_LOGGING_NORMAL(("dllname conversion length check failed (WTF?)"));
+			goto fallback;
+		}
+		dllnameW = new wchar_t[len];
+		if (MultiByteToWideChar(CP_ACP, 0, dllname, -1, dllnameW, len) == 0)
+		{
+			DEBUG_LOGGING_NORMAL(("dllname conversion failed (WTF?)"));
+			goto error_free_wstr;
+		}
+#endif 
+		int result = MH_CreateHookApiEx(dllnameW, exportname, ProxyFunction, pOriginalFunction, &ppTarget);
+		if (result != MH_OK)
+		{
+			DEBUG_LOGGING_NORMAL(("MH_CreateHookApiEx() failed (%d)", result));
+			goto error_free_wstr;
+		}
+		result = MH_EnableHook(ppTarget);
+		if (result != MH_OK)
+		{
+			DEBUG_LOGGING_NORMAL(("MH_EnableHook failed (%d)", result));
+			goto error_free_wstr;
+		}
+		else
+		{
+			DEBUG_LOGGING_NORMAL(("success"));
+			return TRUE;
+		}
+	error_free_wstr:
+#ifdef UNICODE
+		delete[] dllnameW;
+#endif
+		;
+	}
+
+fallback:
+	// fall back to old code, probably don't need this but it's there
+	DEBUG_LOGGING_NORMAL(("Trying fallback"));
+
 	std::stringstream fullpath;
 
 	TCHAR systemdir[MAX_PATH];
@@ -23,8 +76,6 @@ BOOL InstallProxyFunction(LPCTSTR dllname,LPCSTR exportname,VOID *ProxyFunction,
 
 	if( !hDll )
 		return result;
-
-	DEBUG_LOGGING_DETAIL(("hook %s\n", dllname));
 
 	BYTE *p = (BYTE*)::GetProcAddress(hDll, exportname);
 
@@ -261,6 +312,12 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 		{
 			TCHAR temppath[MAX_PATH];
 			::DisableThreadLibraryCalls( hModule );
+
+			if (MH_Initialize() != MH_OK)
+			{
+				DEBUG_LOGGING_NORMAL(("MH_Initialize() failed, falling back to old DLL proxy code"));
+			}
+
 			CreateTinyConsole();
 			OpenSharedMemory();
 #ifdef USE_WS2_32DLLINJECTION
--- Injection/stdafx.h
+++ Injection/stdafx.h
@@ -46,3 +46,5 @@
 #include <fstream>
 
 #include <cwchar>
+
+#include "MinHook.h"
